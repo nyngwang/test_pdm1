0f67fadc27ffb57e435c6e498ee5a134d3196e69
Ning Wang <nyngwang@gmail.com>
2022-03-01 06:15:17 +0800

    test PEP 328 ;)

---
+++ lib/test2.py
@@ -0,0 +1,6 @@
+from .test import foo
+
+
+def foo_enhanced():
+    foo()
+    print('and enhanced by test2:foo_enhanced')
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 from lib import test
+from lib import test2
 
 
 def main():
     print("Hello World! PDM 3")
     print("$FOO is", os.environ["FOO"])
     test.foo()
+    test2.foo_enhanced()
 
 
 if __name__ == "__main__":
