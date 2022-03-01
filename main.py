ea2027e94806c4e5b8c135ca69d6a45e88c557e1
Ning Wang <nyngwang@gmail.com>
2022-03-01 13:44:16 +0800

    practice dict usage use both: `**dict`, `*dict` to unpack

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from lib import test_lib
 def main():
     test_env.from_toml()
     test_lib.test_emulated_switch(10)
+    print('test **dict: %s' % test_lib.emulated_switch(**{'comp': 3}))
+    print('test *dict: %s' % test_lib.emulated_switch(*{'comp': 3}))
 
 
 if __name__ == "__main__":
