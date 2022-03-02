08e88c077c44c538a4042960fba184c77eab3445
Ning Wang <nyngwang@gmail.com>
2022-03-02 16:38:43 +0800

    改用 diagnosticls: 使用 `flake8`(linter), [`black`,`isort`] formatter (成功), 更新 toml for pyright

--- lib/test_requests.py
+++ lib/test_requests.py
@@ -1,8 +1,10 @@
 import requests
 
+from .test_lib import emulated_switch
 DEFAULT_URL = "https://xkcd.com/1906/"
 
 
 def get_url(url=DEFAULT_URL):
+    emulated_switch(2)
     return requests.get(url)
 
